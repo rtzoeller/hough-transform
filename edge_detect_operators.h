3ac8bd0a6f107ef3f0c31bf4c6cdcc837e9866f4
Ryan Zoeller <rtzoeller@gmail.com>
2015-12-14 11:07:28 -0600

    Added definitions of Prewitt and Scharr operators

--- edge_detect_operators.h
+++ edge_detect_operators.h
@@ -7,18 +7,54 @@
 template<typename T>
 std::unique_ptr<arma::Mat<T>> get_sobel_x() {
     std::unique_ptr<arma::Mat<T>> Gx = std::make_unique<arma::Mat<T>>(3, 3);
-    *Gx << -1 << 0 << 1 << arma::endr
-    << -2 << 0 << 2 << arma::endr
-    << -1 << 0 << 1 << arma::endr;
+    *Gx = {{-1, 0, 1},
+           {-2, 0, 2},
+           {-1, 0, 1}};
     return Gx;
 }
 
 template<typename T>
 std::unique_ptr<arma::Mat<T>> get_sobel_y() {
     std::unique_ptr<arma::Mat<T>> Gy = std::make_unique<arma::Mat<T>>(3, 3);
-    *Gy << -1 << -2 << -1 << arma::endr
-    << 0 << 0 << 0 << arma::endr
-    << 1 << 2 << 1 << arma::endr;
+    *Gy = {{-1, -2, -1},
+           {0,  0,  0},
+           {1,  2,  1}};
+    return Gy;
+}
+
+template<typename T>
+std::unique_ptr<arma::Mat<T>> get_scharr_x() {
+    std::unique_ptr<arma::Mat<T>> Gx = std::make_unique<arma::Mat<T>>(3, 3);
+    *Gx = {{-3,  0, 3},
+           {-10, 0, 10},
+           {-3,  0, 3}};
+    return Gx;
+}
+
+template<typename T>
+std::unique_ptr<arma::Mat<T>> get_scharr_y() {
+    std::unique_ptr<arma::Mat<T>> Gy = std::make_unique<arma::Mat<T>>(3, 3);
+    *Gy = {{-3, -10, -3},
+           {0,  0,   0},
+           {3,  10,  3}};
+    return Gy;
+}
+
+template<typename T>
+std::unique_ptr<arma::Mat<T>> get_prewitt_x() {
+    std::unique_ptr<arma::Mat<T>> Gx = std::make_unique<arma::Mat<T>>(3, 3);
+    *Gx = {{-1, 0, 1},
+           {-1, 0, 1},
+           {-1, 0, 1}};
+    return Gx;
+}
+
+template<typename T>
+std::unique_ptr<arma::Mat<T>> get_prewitt_y() {
+    std::unique_ptr<arma::Mat<T>> Gy = std::make_unique<arma::Mat<T>>(3, 3);
+    *Gy = {{-1, -1, -1},
+           {0,  0,  0},
+           {1,  1,  1}};
     return Gy;
 }
 
