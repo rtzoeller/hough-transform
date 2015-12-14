c132972403ee7a665d275889a5ff09d71d0f0c99
Ryan Zoeller <rtzoeller@gmail.com>
2015-12-14 12:37:20 -0600

    Moved image convolution to edge detection file

--- main.cpp
+++ main.cpp
@@ -2,83 +2,8 @@
 #include <opencv2/opencv.hpp>
 #include <memory>
 #include <chrono>
-#include "edge_detect_operators.h"
+#include "edge_detection.h"
 #include "hough_transform.h"
-
-template<typename T>
-std::unique_ptr<arma::Mat<T>> convolve(const arma::Mat<T> &matrix, const arma::Mat<T> &kernel) {
-    // TODO: Specialize for separable kernels
-    unsigned long long Mm, Mn, Km, Kn, Kcx, Kcy, mm, nn, ii, jj;
-    Mm = matrix.n_rows;
-    Mn = matrix.n_cols;
-    Km = kernel.n_rows;
-    Kn = kernel.n_cols;
-    Kcx = Kn / 2;
-    Kcy = Km / 2;
-
-    std::unique_ptr<arma::Mat<T>> result = std::make_unique<arma::Mat<T>>(arma::size(matrix));
-
-    for (unsigned long long i = 0; i < Mm; i++) {
-        for (unsigned long long j = 0; j < Mn; j++) {
-            T sum = 0;
-            for (unsigned long long y = 0; y < Km; y++) {
-                mm = Km - 1 - y;
-                for (unsigned long long x = 0; x < Kn; x++) {
-                    nn = Kn - 1 - x;
-                    ii = i + (y - Kcy);
-                    jj = j + (x - Kcx);
-
-                    if (ii >= 0 && ii < Mm && jj >= 0 && jj < Mn) {
-                        // Normal case, we aren't trying to read from an invalid index
-                        sum += matrix(ii, jj) * kernel(mm, nn);
-
-                    } else {
-                        // We are trying to read from an invalid index, so we extend the last valid element outward
-                        if (ii < 0) {
-                            if (jj < 0) {
-                                // Top left
-                                sum += matrix(0, 0) * kernel(mm, nn);
-                            } else if (jj >= Mn) {
-                                // Top right
-                                sum += matrix(0, Mn - 1) * kernel(mm, nn);
-                            } else {
-                                // Top middle
-                                sum += matrix(0, jj) * kernel(mm, nn);
-                            }
-                        } else if (ii >= Mm) {
-                            if (jj < 0) {
-                                // Bottom left
-                                sum += matrix(Mm - 1, 0) * kernel(mm, nn);
-                            } else if (jj >= Mn) {
-                                // Bottom right
-                                sum += matrix(Mm - 1, Mn - 1) * kernel(mm, nn);
-                            } else {
-                                // Bottom middle
-                                sum += matrix(Mm - 1, jj) * kernel(mm, nn);
-                            }
-                        } else if (jj < 0) {
-                            if (ii >= 0 && ii < Mm) {
-                                // Left middle
-                                // We don't handle left top or bottom because those are handled above
-                                sum += matrix(ii, 0) * kernel(mm, nn);
-                            }
-                        } else if (jj >= Mn) {
-                            if (ii >= 0 && ii < Mm) {
-                                // Right middle
-                                // We don't handle right top or bottom because those are handled above
-                                sum += matrix(ii, Mm - 1) * kernel(mm, nn);
-                            }
-                        }
-
-                    }
-                }
-            }
-            (*result)(i, j) = sum;
-        }
-    }
-
-    return result;
-}
 
 template<typename T>
 std::unique_ptr<arma::Mat<T>> open_image_grayscale(std::string filename) {
