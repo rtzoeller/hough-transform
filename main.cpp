cafb661e31d3dbcba0efdc9c483d38490bb3adce
Ryan Zoeller <rtzoeller@gmail.com>
2015-12-16 12:30:52 -0600

    Moved loading/saving images to a seperate file

--- main.cpp
+++ main.cpp
@@ -4,29 +4,7 @@
 #include <chrono>
 #include "edge_detection.h"
 #include "hough_transform.h"
-
-template<typename T>
-std::unique_ptr<arma::Mat<T>> open_image_grayscale(std::string filename) {
-    // Armadillo stores matrices in column major order, OpenCV in row major order,
-    // so we have to transpose the matrix before handing it to Armadillo.
-    cv::Mat image = cv::imread(filename, CV_LOAD_IMAGE_GRAYSCALE);
-    image = image.t();
-    arma::Mat<uint8_t> output(image.ptr(), image.size().width, image.size().height);
-    return std::make_unique<arma::Mat<T>>(arma::conv_to<arma::Mat<T>>::from(output));
-}
-
-template<typename T>
-void save_image_grayscale(const arma::Mat<T> &matrix, std::string filename) {
-    // Scale down to a 0-255 range
-
-    arma::Mat<T> scaled(matrix);
-    scaled *= 255;
-    scaled /= matrix.max();
-    arma::Mat<uint8_t> bytes = arma::conv_to<arma::Mat<uint8_t>>::from(scaled);
-    bytes = bytes.t();
-    cv::Mat image(bytes.n_cols, bytes.n_rows, CV_8UC1, bytes.memptr());
-    cv::imwrite(filename, image);
-}
+#include "image_io.h"
 
 void print_timestamped(std::string message, std::chrono::steady_clock::time_point time_zero) {
     std::chrono::steady_clock::time_point current = std::chrono::steady_clock::now();
